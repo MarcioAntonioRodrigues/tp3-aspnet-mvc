fbdfa02188641eeffcfd9afa11133f41815b2274
Marcio Antonio Rodrigues de Souza <marcio.dsouza@al.infnet.edu.br>
2019-08-30 21:33:01 -0300

    terceiro commit

--- tpAspNet/Controllers/AniversarioController.cs
+++ tpAspNet/Controllers/AniversarioController.cs
@@ -32,6 +32,7 @@ namespace tpAspNet.Controllers
         [HttpPost]
         public ActionResult Create(Pessoa pessoa)
         {
+            pessoa.PessoaId = this.createId();
             PessoaDal.addPessoa(pessoa);
             return RedirectToAction("Index");
         }
@@ -39,13 +40,22 @@ namespace tpAspNet.Controllers
         // GET: Aniversario/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(buscarId(id));
         }
 
         // POST: Aniversario/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(int id, Pessoa person)
         {
+            foreach (Pessoa pessoa in PessoaDal.listarPessoas())
+            {
+                if(pessoa.PessoaId == id)
+                {
+                    //person.Nome = 
+                }
+
+            }
+
             try
             {
                 // TODO: Add update logic here
@@ -78,6 +88,42 @@ namespace tpAspNet.Controllers
             {
                 return View();
             }
+        }
+
+        public Pessoa buscarId(int id)
+        {
+            Pessoa resultado = new Pessoa();
+            foreach(Pessoa p in PessoaDal.listarPessoas())
+            {
+                if(p.PessoaId == id)
+                {
+                    resultado.PessoaId = id;
+                    resultado.Nome = p.Nome;
+                    resultado.DataAniversario = p.DataAniversario;
+                    break;
+                }
+            }
+            return resultado;
+        }
+
+        public void EditPessoa(int id, Pessoa p)
+        {
+            foreach(Pessoa pessoa in PessoaDal.listarPessoas())
+            {
+                if(pessoa.PessoaId == id)
+                {
+                    pessoa.Nome = p.Nome;
+                    pessoa.SobreNome = p.SobreNome;
+                    pessoa.DataAniversario = p.DataAniversario;
+                    break;
+                }
+            }
+        }
+
+        public int createId()
+        {
+            int count = 0;
+            return count +=1;
         }
     }
 }
--- tpAspNet/Models/PessoaDal.cs
+++ tpAspNet/Models/PessoaDal.cs
@@ -17,17 +17,19 @@ namespace tpAspNet.Models
                 p1.PessoaId = 1;
                 p1.Nome = "Marcio";
                 p1.SobreNome = "Souza";
-                p1.DataAniversario = new DateTime(19/02/1984);
+                p1.DataAniversario = new DateTime(02/19/1984);
 
                 Pessoa p2 = new Pessoa();
+                p2.PessoaId = 2;
                 p2.Nome = "Marcio";
                 p2.SobreNome = "Souza";
-                p2.DataAniversario = new DateTime(19 / 02 / 1984);
+                p2.DataAniversario = new DateTime(19/02/1984);
 
                 Pessoa p3 = new Pessoa();
+                p3.PessoaId = 3;
                 p3.Nome = "Marcio";
                 p3.SobreNome = "Souza";
-                p3.DataAniversario = new DateTime(19 / 02 / 1984);
+                p3.DataAniversario = new DateTime(19/02/1984);
 
                 listaPessoa.Add(p1);
                 listaPessoa.Add(p2);
